f9277c187757c45bd246a686931eb4e8dda8ee59
Godfraind <nicolas.godfraind@gmail.com>
2015-03-31 17:34:18 +0200

    Create LoadParametersData.php

---
+++ DataFixtures/LoadParametersData.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FormaLibre\WorkspaceSubscriptionBundle\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use FormaLibre\InvoiceBundle\Entity\Product;
+use FormaLibre\InvoiceBundle\Entity\PriceSolution;
+
+class LoadParametersData extends AbstractFixture implements ContainerAwareInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $configHandler = $this->container->get('claroline.config.platform_config_handler');
+        $configHandler->setParameter('formalibre_decrypt', true);
+        $configHandler->setParameter('formalibre_encryption_secret_decrypt', 'bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3'); //change me bro
+    }
+}
